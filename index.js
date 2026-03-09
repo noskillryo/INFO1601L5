c9e76a8e9e23f565a35b7eb2e86d833c6c151cba
noskillryo <noskillryo@gmail.com>
2026-03-09 13:35:26 -0400

    4.3

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log('1' === 1);
-console.log(1 === true); 
-console.log("true" === true); 
+for(let i=0; i< 50; i++){
+  if(i %3 === 0 && i%5 === 0){ 
+    console.log('fizzbuzz');
+  }
+  else if(i%3 === 0){
+    console.log('fizz');
+  }
+  else if(i%5 === 0){ 
+    console.log('buzz');
+  }
+  else{          
+    console.log(i);
+  }
+}
