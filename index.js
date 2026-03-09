465389ae32d94990faacdd68bdeb091db6bcceaa
noskillryo <noskillryo@gmail.com>
2026-03-09 13:32:52 -0400

    4.2

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-console.log('1'==1); 
-console.log(1==true);
-console.log("false"==false);
-console.log("false"==true); 
-
-if("false")
-   console.log("Hello false!!")
+console.log('1' === 1);
+console.log(1 === true); 
+console.log("true" === true); 
